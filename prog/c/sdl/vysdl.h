df7c6815721068c9583b601035fa76d8197e99e6
Várnagy Zoltán <doi6doi@gmail.com>
2024-05-01 13:42:54 -0600

    fejl

---
+++ prog/c/sdl/vysdl.h
@@ -0,0 +1,11 @@
+#ifndef VYSDLH
+#define VYSDLH
+
+#include "vy.h"
+
+void vySdlInitKey( VyContext );
+void vySdlInitView( VyContext );
+void vySdlInitWindow( VyContext );
+void vySdlInitSprite( VyContext );
+
+#endif // VYSDLH

599e7a7a8e8460b6bb9d1c6a0f0fc6f967a130e4
Várnagy Zoltán <doi6doi@gmail.com>
2024-06-01 09:19:50 -0600

    fejl

---
+++ prog/php/vymake/CCompiler.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace vy;
+
+/// C fordító
+abstract class CCompiler {
+   
+   const
+      MSVC = "msvc",
+      GCC = "gcc",
+      CLANG = "clang",
+      CUSTOM = "custom";
+   
+   /// fordító gyártása
+   static function create( $kind=null ) {
+      if ( ! $kind )
+         $kind = self::defaultKind();
+      switch ($kind) {
+         case self::GCC: return new Gcc();
+         default: throw new EVy("Unknown compiler: $kind");
+      }
+   }
+
+   /// alap típus a rendszerben
+   static function defaultKind() {
+      switch ( Tools::system() ) {
+         case Tools::WINDOWS: return self::MSVC;
+         default: return self::GCC;
+      }
+   }
+   
+   /// parancsok kiírása
+   protected $show;
+   
+   /// depend fájl készítés
+   abstract function depend( $dst, $src );
+
+   /// object fáljok linkelése könyvtárrá
+   abstract function linkLib( $dst, $src );
+
+   /// dep fájl felolvasása 
+   abstract function loadDep( $fname );
+
+   /// object fájl készítése
+   abstract function compile( $dst, $src );
+
+   /// a futtatható program
+   abstract function executable();
+
+   /// a futtatható program
+   abstract function setIncDir( $dir ); 
+
+   function setShow( $value ) { $this->show = $value; }
+
+   /// fordító futtatása
+   protected function run($fmt) {
+      $args = call_user_func_array( "sprintf", func_get_args() );
+      return $this->exec( $this->executable(), $args );
+   }
+
+   /// külső program futtatása
+   protected function exec( $prg, $args ) {
+      $out = [];
+      $cmd = sprintf( "%s %s", $this->esc($prg), $args );
+      if ( $this->show ) {
+         print( "$cmd\n" );
+         $r = passthru( $cmd, $rv );
+         $outs = "";
+      } else {
+         $cmd .= " 2>&1";
+         $r = exec( $cmd, $out, $rv );
+         $outs = implode("\n",$out);
+      }
+      if ( (false === $r) || (0 != $rv) )
+         throw new EVy("Exec error: $rv $outs");
+      return $outs;
+   }
+   
+   /// parancssori escape
+   protected function esc( $x ) {
+      if ( ! $x )
+         return "";
+      if ( ! is_array( $x ))
+         $x = [$x];
+      $ret = "";
+      foreach ( $x as $i )
+         $ret .= " ".escapeshellarg($i);
+      return trim($ret);
+   }
+   
+}
---
+++ prog/php/vymake/Gcc.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace vy;
+
+class Gcc extends CCompiler {
+ 
+   protected $inc;
+ 
+   function executable() { return "gcc"; }
+   
+   function depend( $dst, $src ) {
+      $this->run( "-MM %s > %s", $this->esc( $src ), $this->esc( $dst ));
+   }
+   
+   function linkLib( $dst, $src ) {
+      $this->run( "-shared -o %s %s", $this->esc($dst), $this->esc($src));
+   }
+ 
+   function compile( $dst, $src ) {
+      $this->run( "%s-o %s %s", $this->incArg(),
+         $this->esc($dst), $this->esc($src));
+   }
+   
+   function setIncDir( $x ) {
+      if ( ! $x )
+         return $this->inc = [];
+      if ( ! is_array( $x ))
+         $x = [$x];
+      $this->inc = $x;
+   }
+   
+   /// include könyvtár parancssori argumentum
+   function incArg() {
+      if ( ! $this->inc ) return "";
+      return sprintf("-I %s ", implode(":",$this->inc));
+   }
+   
+   function loadDep( $fname ) {
+      $ret = [];
+      $data = Tools::loadFile( $fname );
+      $rows = explode("\n",$data);
+      $dst = null;
+      foreach ($rows as $r) {
+         if ( ! $r ) break;
+         if ( ! preg_match('#^((.*):)?(.*?)(\\\\?)$#', $r, $m ))
+            throw new EVy("Unknown dep line: $r");
+         if ( $m[2] ) {
+            $dst = $m[2];
+            $ret[$dst] = [];
+         }
+         $ret[$dst] = array_merge( $ret[$dst], explode(" ", $m[3] ) );
+         if ( ! $m[4] )
+            $dst = null;
+     }
+     return $ret;
+   }
+}

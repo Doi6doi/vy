bea4e862a1d610d3e8e29352218bffc12f6086ca
Várnagy Zoltán <doi6doi@gmail.com>
2025-02-24 09:28:48 -0600

    fejl

---
+++ prog/php/vymake/MakeImportCmd.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace vy;
+
+/// parancssori eszköz importja
+abstract class MakeImportCmd extends MakeImport {
+   
+   function __construct( $owner, $name ) {
+	   parent::__construct( $owner, $name );
+ 	   $this->addFuncs( ["get","set"] );
+   }
+
+   /// a parancssori eszköz objektuma
+   abstract function cmd();
+
+   /// változó beállítása
+   function set( $fld, $val ) {
+      $this->cmd()->set( $fld, $val );
+   }
+   
+   /// változó lekérdezése
+   function get( $fld ) {
+      return $this->cmd->get( $fld );
+   }
+   
+}
